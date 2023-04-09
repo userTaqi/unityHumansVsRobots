f0922d7f779b003872a43d6a30ee4923ffa29b3e
Taqi Hasan <92113141+TaQi30@users.noreply.github.com>
2023-04-09 06:21:29 +0100

    soldier shoots

--- Assets/Resources/scripts/SoldierShooting.cs
+++ Assets/Resources/scripts/SoldierShooting.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class SoldierShooting : MonoBehaviour
 {
-    
+    public GameObject bulletPrefab;  // the prefab of the bullet to shoot
+    public float bulletSpeed = 10f;  // the speed at which to shoot the bullet
+    public float shootInterval = 1f; // the time between shots
+
+    private float timeSinceLastShot = 0f;
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+
+        // check if it's time to shoot again
+        if (timeSinceLastShot >= shootInterval)
+        {
+            Shoot();
+            timeSinceLastShot = 0f;
+        }
+    }
+
+    private void Shoot()
+    {
+        // instantiate the bullet prefab
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        // shoot the bullet to the right
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.right * bulletSpeed;
+    }
 }
