56233127884f1b3ed6ce7c32a7667c867ab9efc6
Taqi Hasan <92113141+TaQi30@users.noreply.github.com>
2023-04-11 11:45:29 +0100

    spawn rates and stuff

--- Assets/Resources/scripts/EnemySpawner.cs
+++ Assets/Resources/scripts/EnemySpawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject[] enemyPrefabs;
     public GameObject[] spawnPoints;
 
     public float spawnInterval = 2f;
-    public int enemyPerWave = 5;
+    private int enemyPerWave = 3;
     public float timeBetweenWaves = 10f;
 
     private int currentWave = 0;
@@ -22,25 +22,61 @@ public class EnemySpawner : MonoBehaviour
     IEnumerator SpawnEnemy()
     {
         isSpawning = true;
+        float currentSpawnInterval = spawnInterval;
 
         while (true)
         {
             yield return new WaitForSeconds(timeBetweenWaves);
-
-            currentWave++;
 
             for (int i = 0; i < enemyPerWave; i++)
             {
                 int randomSpawnPointIndex = Random.Range(0, spawnPoints.Length);
                 GameObject spawnPoint = spawnPoints[randomSpawnPointIndex];
 
-                GameObject enemy = Instantiate(enemyPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                enemy.GetComponent<Rigidbody2D>().AddForce(-spawnPoint.transform.right * 70f);
+                int strengthLevel = Mathf.FloorToInt(currentWave / 2f); // Increase strength level every 5 waves
+                strengthLevel = Mathf.Clamp(strengthLevel, 0, enemyPrefabs.Length - 1); // Clamp strength level to array bounds
 
+                // Create a list of enemy prefabs that can spawn at the current strength level, with the desired spawn rates
+                List<GameObject> availableEnemies = new List<GameObject>();
+                for (int j = 0; j <= strengthLevel; j++)
+                {
+                    if (j == 0)
+                    {
+                        // Add enemyPrefabs[0] with 70% spawn rate
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                        availableEnemies.Add(enemyPrefabs[0]);
+                    }
+                    else if (j == 1)
+                    {
+                        // Add enemyPrefabs[1] with 30% spawn rate
+                        availableEnemies.Add(enemyPrefabs[1]);
+                        availableEnemies.Add(enemyPrefabs[1]);
+                    }
+                    else
+                    {
+                        availableEnemies.Add(enemyPrefabs[j]);
+                    }
+                }
 
-                yield return new WaitForSeconds(spawnInterval);
+                // Randomly select an enemy prefab from the available enemies list
+                GameObject enemyPrefabToSpawn = availableEnemies[Random.Range(0, availableEnemies.Count)];
+
+                GameObject enemy = Instantiate(enemyPrefabToSpawn, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                enemy.GetComponent<Rigidbody2D>().AddForce(-spawnPoint.transform.right * 30f);
+
+                yield return new WaitForSeconds(currentSpawnInterval);
             }
+
+            currentWave++;
+            enemyPerWave++;
+            currentSpawnInterval *= 0.98f; // Gradually decrease spawn interval by 2% every wave
         }
     }
-}
 
+}
